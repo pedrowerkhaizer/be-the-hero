6a4853be2d1345a70c2967a401652e11c4101741
Pedro Werkhaizer <pedrowerkhaizer@users.noreply.github.com>
2020-03-26 01:03:00 -0300

    Added new incidents register function

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import { FiArrowLeft } from 'react-icons/fi'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
 
-import './styles.css'
-import logoImg from '../../assets/logo.svg'
+import api from '../../services/api';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
 
 export default function NewIncident() {
+    const history = useHistory();
+
+    const [ title, setTitle ] = useState('');
+    const [ description, setDescription ] = useState('');
+    const [ value, setValue ] = useState('');
+
+    async function handleNewIncident(e) {
+        e.preventDefault();
+
+        const ongId = localStorage.getItem('ongId');
+
+        const data = {
+            title,
+            description,
+            value
+        };
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+            history.push('/profile');
+            alert('Parabéns!! Caso cadastrado com sucesso :)');
+        } catch (err) {
+            alert('Não foi possivel cadastrar esse caso. Tente novamente mais tarde.');
+        };
+    }
+
     return (
         <div className="new-incident-container">
             <div className="content">
@@ -21,10 +52,19 @@ export default function NewIncident() {
                         </Link>
 
                 </section>
-                <form>
-                    <input placeholder="Título do caso" type="text"/>
-                    <textarea placeholder="Descrição" />
-                    <input placeholder="Valor em reais" type="text"/>
+                <form onSubmit={handleNewIncident} >
+                    <input 
+                    placeholder="Título do caso"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)} />
+                    <textarea 
+                    placeholder="Descrição" 
+                    value={description}
+                    onChange={e => setDescription(e.target.value)} />
+                    <input 
+                    placeholder="Valor em reais" 
+                    value={value}
+                    onChange={e => setValue(e.target.value)} />
 
                     <div className="option-container">
                        <Link className="button" to="/profile" >Cancelar</Link>
